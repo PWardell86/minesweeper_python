ac476b94c4070b29386ac869fd13249f7d2b0fc4
ppeter0 <wardelp@mcmaster.ca>
2022-05-28 15:58:22 -0400

    Added comments

--- Tile.py
+++ Tile.py
@@ -1,12 +1,18 @@
-from pyglet import sprite
+from pyglet import sprite, graphics
 class Tile(sprite.Sprite):
     def __init__(self, x, y, size, defaultImage, batch, value=None, isRevealed=False, isFlagged=False):
+        """
+        Defines a tile for the game
+            - isRevealed --> if the tile has been clicked / revealed
+            - isFlagged --> if the tile is flagged
+            - value --> what the value of the tile is: 
+                - 0 - 8 are regular values
+                - 9 is a bomb
+        """
         self.isRevealed = isRevealed
         self.isFlagged = isFlagged
         self.value = value
-        # defaultImage.anchor_x = defaultImage.width / 2
-        # defaultImage.anchor_y = defaultImage.height / 2
-        super(Tile, self).__init__(defaultImage, x, y, batch=batch)
+        super(Tile, self).__init__(defaultImage, x, y, batch=batch, group=graphics.OrderedGroup(-1))
         
         # Scale the tile properly
         self.scale = size / self.width
