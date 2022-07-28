1dae19c1294dcf9f86aab9d524d4c1202c4f7007
Peter <peterwardell86@gmail.com>
2022-07-27 22:30:47 -0400

    Project cleanup

--- src/game/Main.py
+++ src/game/Main.py
@@ -1,7 +1,13 @@
 from MinesweeperMVC import Minesweeper
-from pyglet import app, gl
+from src.bot.SimpleBot import SimpleBot
+from pyglet import app, gl, clock
+playWithBot = True
 
 game = Minesweeper(gameSize=(20, 15))
+if playWithBot:
+    bot = SimpleBot(game)
+    bot.start()
+    clock.schedule_interval(bot.tick, 1)
 
 # Remove aliasing
 gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
