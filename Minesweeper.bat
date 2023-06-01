85fe9960be3b2755358136cf0846482eb319e4eb
Peter Wardell <peter.wardell@agfa.com>
2023-06-01 12:46:47 -0400

    Add build file and update run file

--- Minesweeper.bat
+++ Minesweeper.bat
@@ -1,1 +1,6 @@
+@echo off
+
+if /I "%1" == "" (@echo off) else ( echo Mode not implemented... ignoring %1)
+
+echo Launching game...
 cd .\src && SET PYTHONPATH=$PYTHONPATH;..\ && python -m main.game && cd ../
